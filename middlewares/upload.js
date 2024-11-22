02fc482333bd88078e27cf5ca1dbcf60e2f964ee
besbesbesbes <smt.bes@gmail.com>
2024-11-22 14:58:50 +0700

    fix upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,7 +1,11 @@
 const path = require("path");
+const fs = require("fs");
 const multer = require("multer");
 
 const uploadDirectory = path.join(process.cwd(), "public", "upload-pic");
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory, { recursive: true });
+}
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, uploadDirectory),
   filename: (req, file, cb) => {
